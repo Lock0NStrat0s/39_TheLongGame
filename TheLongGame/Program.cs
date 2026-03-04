c04b09cd655e136ab6d589c7e0fa88429e35fb5e
Mohammad Jabir <mjabir94@proton.me>
2026-03-04 16:20:09 -0700

    initial commit.

---
+++ TheLongGame/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TheLongGame;
+
+/*Objectives:
+    • When the program starts, ask the user to enter their name.
+    • By default, the player starts with a score of 0.
+    • Add 1 point to their score for every keypress they make.
+    • Display the player’s updated score after each keypress.
+    • When the player presses the Enter key, end the game. Hint: the following code reads a keypress and
+    checks whether it was the Enter key: Console.ReadKey().Key == ConsoleKey.Enter
+    • When the player presses Enter, save their score in a file. Hint: Consider saving this to a file named
+    [username].txt. For this challenge, you can assume the user doesn’t enter a name that would produce
+    an illegal file name (such as *).
+    • When a user enters a name at the start, if they already have a previously saved score, start with that
+score instead.*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
